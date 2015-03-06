fdcb8fae51378e3e93045f40e721ffb090a0daea
Bree Stanwyck <bree@bignerdranch.com>
2015-03-06 17:04:29 -0500

    Uses match data for index/post

--- src/Main.hs
+++ src/Main.hs
@@ -1,6 +1,12 @@
+import Control.Arrow
 import Control.Monad.IO.Class
 import Data.Aeson
+import qualified Data.ByteString.Char8 as B
 import Data.ByteString (ByteString)
+import Data.ByteString.Lazy (toStrict)
+import Data.Maybe
+import qualified Data.Map as Map
+import Data.Monoid ((<>))
 import Data.Proxy
 import Data.Text (Text)
 import Database.Redis (Redis, Reply, runRedis)
@@ -16,52 +22,62 @@ import qualified Data.Text.IO as T
 import qualified Data.Text.Encoding as T
 
 
-newtype Note = Note
-    { contents :: Text
+data Match = Match
+    { guid :: Text
+    , creator_id :: Text
+    , opponent_id :: Text
+    , creator_score :: Int
+    , opponent_score :: Int
     }
   deriving (Generic, Show)
 
-instance FromJSON Note
-instance ToJSON Note
+instance FromJSON Match
+instance ToJSON Match
 
-
-getNotes :: MonadIO m => Redis.Connection -> m [Note]
-getNotes redisConn = do
-    let redisCmd = Redis.lrange "foo" 0 (-1) :: Redis (Either Reply [ByteString])
-    ts' <- liftIO (runRedis redisConn redisCmd)
-    let ts = either (error . show) id ts'
-    return $ map (Note . T.decodeUtf8) ts
-
-postNote :: MonadIO m => Redis.Connection -> Note -> m [Note]
-postNote redisConn note =
-    liftIO $ do
-      T.putStrLn $ contents note
-      _ <- liftIO $ runRedis redisConn $ Redis.lpush "foo" [T.encodeUtf8 $ contents note]
-      return []
-
-
-type NoteAPI =
+type MatchAPI =
          Get Text
-    :<|> "notes" :> Get [Note]
-    :<|> "notes" :> ReqBody Note :> Post [Note]
+    :<|> "matches" :> Get [Match]
+    :<|> "match" :> ReqBody Match :> Post Match
 
-noteAPI :: Proxy NoteAPI
+noteAPI :: Proxy MatchAPI
 noteAPI =
     Proxy
 
-server :: Text -> Redis.Connection -> Server NoteAPI
+getMatches :: MonadIO m => Redis.Connection -> m [Match]
+getMatches redisConn = do
+    let redisCmd = Redis.smembers "matches"
+    matchPkeys' <- liftIO (runRedis redisConn redisCmd)
+    let matchPkeys = either (error . show) id matchPkeys'
+    mapM pkeyToMatch matchPkeys
+    where pkeyToMatch pkey = do
+            Right attrs' <- liftIO $ runRedis redisConn $ Redis.hmget pkey ["creator_id", "opponent_id", "creator_score", "opponent_score"]
+            let [cid, oid, cs, os] = catMaybes attrs'
+            return $ Match (T.decodeUtf8 pkey) (T.decodeUtf8 cid) (T.decodeUtf8 oid) (read . B.unpack $ cs) (read . B.unpack $ os)
+
+postMatch :: MonadIO m => Redis.Connection -> Match -> m Match
+postMatch redisConn match =
+    liftIO $ do
+      let attrs = [ ("creator_id", creator_id match)
+                  , ("opponent_id", opponent_id match)
+                  , ("creator_score", T.pack . show $ creator_score match)
+                  , ("opponent_score", T.pack . show $ opponent_score match)
+                  ]
+          pkey = "match:" <> T.encodeUtf8 (guid match)
+      _ <- liftIO $ runRedis redisConn $ Redis.hmset pkey (map (id *** T.encodeUtf8) attrs)
+      _ <- liftIO $ runRedis redisConn $ Redis.sadd "matches" [pkey]
+      return match
+
+server :: Text -> Redis.Connection -> Server MatchAPI
 server home conn =
          return home
-    :<|> getNotes conn
-    :<|> postNote conn
-
+    :<|> getMatches conn
+    :<|> postMatch conn
 
 main :: IO ()
 main = do
     hSetBuffering stdout LineBuffering
     env <- getEnvironment
     let port = maybe 8080 read $ lookup "PORT" env
-        home = maybe "Welcome to Haskell on Heroku" T.pack $
-                 lookup "TUTORIAL_HOME" env
+        home = T.decodeUtf8 . toStrict . encode $ Map.fromList [("success" :: Text, True)]
     redisConn <- Redis.connect Redis.defaultConnectInfo
     run port $ serve noteAPI $ server home redisConn
